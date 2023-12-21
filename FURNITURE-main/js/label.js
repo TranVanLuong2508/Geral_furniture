deb838482ce2dad9867553a9541ec325bd73a0b2
Luong2508 <tranvanluong032020@gmai.com>
2023-12-21 14:05:55 +0700

    add project

---
+++ FURNITURE-main/js/label.js
@@ -0,0 +1,238 @@
+var labels={
+    "MENU_HOME":{
+        "vi-VN":"Trang Chủ",
+        "en-US" : "Home",
+    },
+
+    "MENU_PRODUCTS":{
+        "vi-VN":"Sản Phẩm",
+        "en-US" : "Products",
+    },
+
+    "MENU_PRODUCTS_1":{
+        "vi-VN":"Phòng Bếp",
+        "en-US" : "Kitchen",
+    },
+    "MENU_PRODUCTS_2":{
+        "vi-VN":"Phòng Khách",
+        "en-US" : "Living Room",
+    },
+    "MENU_PRODUCTS_3":{
+        "vi-VN":"Phòng Ngủ",
+        "en-US" : "BedRoom",
+    },
+    "MENU_PRODUCTS_4":{
+        "vi-VN":"Phòng Làm Việc",
+        "en-US" : "Workroom",
+    },
+
+    
+    "MENU_LOVE_PRODUCT":{
+        "vi-VN":"Sản Phẩm Yêu Thích",
+        "en-US" : "Love Products",
+    },
+
+    "MENU_HOME_SERVICE":{
+        "vi-VN":"Dịch vụ",
+        "en-US" : "Service",
+    },
+
+    "MENU_SERVICE_1":{
+        "vi-VN":"Chính Sách Bán Hàng",
+        "en-US" : "Sales Policy",
+    },
+
+
+    "MENU_SERVICE_2":{
+        "vi-VN":"Chính Sách Lắp Giao Hàng & Lắp Đặt",
+        "en-US" : "Delivery & Installation Policy",
+    },
+
+    "MENU_SERVICE_3":{
+        "vi-VN":"Chính Sách Đổi Trả",
+        "en-US" : "Return Policy",
+    },
+
+    "MENU_SERVICE_4":{
+        "vi-VN":"Chính Sách Bảo Hành & Bảo Trì",
+        "en-US" : "Warranty & Maintenance Policy",
+    },
+
+    "MENU_SERVICE_5":{
+        "vi-VN":"Khách Hàng Thân Thiết - G.VIP",
+        "en-US" : "Loyal Customers - G.VIP",
+    },
+
+
+    "MAIN_TITLE":{
+        "vi-VN":"Chào mừng đến với GERAL <br> Cộng đồng Nơi Bạn Thuộc Về",
+        "en-US" : "Welcome To Geral - Where You Been",
+    },
+
+    "MAIN_CONTENT":{
+        "vi-VN":"Khi bạn gia nhập GERAL, bạn không chỉ đơn thuần trở thành một thành viên, mà còn trở thành một phần của cộng đồng đam mê và sự sáng tạo. Đây không chỉ là một trải nghiệm mua sắm, mà là một hành trình khám phá vẻ đẹp và sự sáng tạo trong không gian sống.",
+        "en-US" : "When you join GERAL, you not only become a member but also become part of a community of passion and creativity. This is not just a shopping experience but a journey to discover beauty and creativity in living spaces.",
+    },
+
+    
+    "MAIN_FORM_TITLE":{
+        "vi-VN":"Đăng Ký ",
+        "en-US" : "Register",
+    },
+
+    "MAIN_FORM_FNAME":{
+        "vi-VN":"Tên",
+        "en-US" : "First Name",
+    },
+
+    "MAIN_FORM_LNAME":{
+        "vi-VN":"Họ",
+        "en-US" : "Last Name",
+    },
+
+    "MAIN_FORM_PASS":{
+        "vi-VN":"Mật Khẩu",
+        "en-US" : "Password",
+    },
+
+    "MAIN_FORM_SUB":{
+        "vi-VN":"Đồng ý với điều khoản.",
+        "en-US" : "Subscribe to our newsletter",
+    },
+
+    "MAIN_FORM_SUBMIT":{
+        "vi-VN":"Đăng Ký ",
+        "en-US" : "Register",
+    },
+
+    "MAIN_FORM_SUBWITH":{
+        "vi-VN":"Hoặc đăng kí với:",
+        "en-US" : "Or Register With: ",
+    },
+
+    //footer
+
+    "MAIN_FOOTER_NAME":{
+        "vi-VN":"Nội thất GERAL",
+        "en-US" : "GERAL FURNITURE",
+    },
+
+    "MAIN_FOOTER_NAME_CONTENT":{
+        "vi-VN":"Nội thất GERAL tuy chỉ là một thương hiệu bán hàng nội thất mới phát triển thời gian gần đây nhưng cam kết sẽ luôn mang đến cho khách hàng những trải nghiệm mua hàng tuyệt vời nhất.",
+        "en-US" : "Although GERAL is a newly emerging furniture brand, it's committed to consistently delivering the best shopping experiences for customers",
+    },
+
+    
+    "MAIN_FOOTER_SER":{
+        "vi-VN":"Dịch vụ",
+        "en-US" : "SERVICE",
+    },
+
+    "MAIN_FOOTER_SER_1":{
+        "vi-VN":"Chính Sách Bán Hàng",
+        "en-US" : "Sales Policy",
+    },
+
+    "MAIN_FOOTER_SER_2":{
+        "vi-VN":"Chính Sách Lắp Giao Hàng & Lắp Đặt",
+        "en-US" : "Delivery & Installation Policy",
+    },
+
+    "MAIN_FOOTER_SER_3":{
+        "vi-VN":"Chính Sách Đổi Trả",
+        "en-US" : "Return Policy",
+    },
+
+
+    "MAIN_FOOTER_SER_4":{
+        "vi-VN":"Khách Hàng Thân Thiết - G.VIP",
+        "en-US" : "Loyal Customers - G.VIP",
+    },
+
+    "MAIN_FOOTER_SER_5":{
+        "vi-VN":"Câu Hỏi thường gặp",
+        "en-US" : "Frequently Asked Questions",
+    },
+
+    "FOOTER_INFOR":{
+        "vi-VN":"Thông tin liên hệ",
+        "en-US" : "Contact Information",
+    },
+
+    "FOOTER_INFOR_1":{
+        "vi-VN":"Trường Đại Học Cần Thơ - Đường 3/2, Phường Xuân Khánh, Quận Ninh Kiều, Thành Phố Cần Thơ.",
+        "en-US" : "CanTho University - 3/2 Street, Xuan Khanh, Ninh Kieu, Can Tho City",
+    },
+
+    "FOOTER_INFOR_2":{
+        "vi-VN":"0122333444455555 (Đội Giao Hàng)",
+        "en-US" : "0122333444455555 (The delivery team)",
+    },
+
+    "FOOTER_PAY":{
+        "vi-VN":"Thanh toán miễn phí",
+        "en-US" : "Payment without fees",
+    },
+
+
+    //main đăng nhập
+
+    "MAIN_TITLE_2":{
+        "vi-VN":"Chào mừng đến với GERAL <br> Nơi Tạo Nên Không Gian Sống Đẳng Cấp!",
+        "en-US" : "Welcome to GERAL - Where Luxury Living Spaces Are Created!",
+    },
+
+    "MAIN_CONTENT_2":{
+        "vi-VN":"GERAL không chỉ đơn thuần là một thương hiệu bán đồ nội thất, mà chúng tôi là người bạn đồng hành tin cậy, mang đến không gian sống hoàn hảo và đẳng cấp cho mỗi không gian trong ngôi nhà của bạn.",
+        "en-US" : "GERAL isn't just a furniture brand; we're your trusted companion, delivering sophisticated and perfect living spaces for every corner of your home.",
+    },
+
+    "MAIN_FORM_TITLE_2":{
+        "vi-VN":"Đăng Nhập ",
+        "en-US" : "Signin",
+    },
+
+    "CREAT":{
+        "vi-VN":"Chưa có tài khoản? Đăng kí ngay",
+        "en-US" : "Not yet registered? Sign up now!",
+    },
+
+    "MAIN_FORM_SUBMIT_2":{
+        "vi-VN":"Đăng Nhập ",
+        "en-US" : "Sign in",
+    },
+
+}
+
+$(document).ready(function()
+{
+    showLabel()
+})
+function setLang(code)
+{
+window.localStorage.setItem("lang",code);
+location.reload();
+}
+
+function getLang()
+{
+if(window.localStorage.getItem("lang")=== null)
+window.localStorage.setItem("lang","vi-VN");
+return window.localStorage.getItem("lang");
+}
+
+function showLabel()
+{
+//var lang="vi-VN";
+var lang=getLang();
+$('.multilang').each(function(i, obj) { $("#"+obj.id).html(labels[obj.id][lang]).attr("title", labels[obj.id][lang]);
+}); 
+}
+
+function regCourse(idx){
+alert(idx)
+}
+
+
+
+  
